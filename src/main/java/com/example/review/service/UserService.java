c4922720d9c9b063eda258c35ecab8d062698792
dmdwns2 <ssreki1@naver.com>
2022-11-29 13:37:22 +0900

    [실습] Spring Security 추가
    build.gradle 추가
    test코드 추가

--- src/main/java/com/example/review/service/UserService.java
+++ src/main/java/com/example/review/service/UserService.java
@@ -7,7 +7,6 @@ import com.example.review.domain.dto.UserJoinRequest;
 import com.example.review.exception.ErrorCode;
 import com.example.review.exception.HospitalReviewAppException;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.BCException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,12 +15,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-
     private final BCryptPasswordEncoder encoder;
+
     public UserDto join(UserJoinRequest request) {
         // 비즈니스 로직 - 회원 가입
-        // 회원 userName(id) 중복 Checkis
+        // 회원 userName(id) 중복 Check
         // 중복이면 회원가입 x --> Exception(예외)발생
+        // 있으면 에러처리
         userRepository.findByUserName(request.getUserName())
                 .ifPresent(user ->{
                     throw new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, String.format("UserName:%s", request.getUserName()));
