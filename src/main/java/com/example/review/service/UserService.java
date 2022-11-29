be4cad062cc7617fab6f83ebb037c5daaf4a91e3
dmdwns2 <ssreki1@naver.com>
2022-11-29 15:20:46 +0900

    [실습] 로그인 기능 추가
    jwt 토큰 유틸 추가
    
    에러코드 INVLID_PASSWORD 추가
    yml, build.gradle 설정 추가
    UserService 기능 추가

--- src/main/java/com/example/review/service/UserService.java
+++ src/main/java/com/example/review/service/UserService.java
@@ -6,7 +6,9 @@ import com.example.review.domain.dto.UserDto;
 import com.example.review.domain.dto.UserJoinRequest;
 import com.example.review.exception.ErrorCode;
 import com.example.review.exception.HospitalReviewAppException;
+import com.example.review.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,25 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
 
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+    private long expireTimeMs = 1000 * 60 * 60; // 1시간
+
+    public String login(String userName, String password) {
+
+        // userName있는지 여부 확인
+        // 없으면 NOT_FOUND에러 발생
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new HospitalReviewAppException(ErrorCode.NOT_FOUND, String.format("%s는 가입된 적이 없습니다.", userName)));
+
+        // password일치 하는지 여부 확인
+        if(!encoder.matches(password, user.getPassword())){
+            throw new HospitalReviewAppException(ErrorCode.INVALID_PASSWORD, String.format("userName 또는 password가 잘못 됐습니다."));
+        }
+
+        // 두가지 확인중 예외 안났으면 Token발행
+        return JwtTokenUtil.createToken(userName, secretKey, expireTimeMs);
+    }
     public UserDto join(UserJoinRequest request) {
         // 비즈니스 로직 - 회원 가입
         // 회원 userName(id) 중복 Check
@@ -35,18 +56,4 @@ public class UserService {
                 .email(savedUser.getEmailAddress())
                 .build();
     }
-    public String login(String userName, String password) {
-
-        // userName있는지 여부 확인
-        // 없으면 NOT_FOUND에러 발생
-        userRepository.findByUserName(userName)
-                .orElseThrow(() -> new HospitalReviewAppException(ErrorCode.NOT_FOUND, String.format("%s는 가입된 적이 없습니다.", userName)));
-
-        // password일치 하는지 여부 확인
-
-        // 두가지 확인중 예외 안났으면 Token발행
-
-        return "";
-    }
-
 }
