155d2427dadcf7c5d5eb049e4ae4e219f6051e6a
dmdwns2 <ssreki1@naver.com>
2022-11-29 13:06:39 +0900

    [실습] Spring Security 추가
    Cofig 추가

--- src/main/java/com/example/review/service/UserService.java
+++ src/main/java/com/example/review/service/UserService.java
@@ -7,6 +7,8 @@ import com.example.review.domain.dto.UserJoinRequest;
 import com.example.review.exception.ErrorCode;
 import com.example.review.exception.HospitalReviewAppException;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.BCException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +17,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final BCryptPasswordEncoder encoder;
     public UserDto join(UserJoinRequest request) {
         // 비즈니스 로직 - 회원 가입
         // 회원 userName(id) 중복 Checkis
@@ -25,7 +28,7 @@ public class UserService {
                 });
 
         // 회원가입 .save()
-        User savedUser = userRepository.save(request.toEntity());
+        User savedUser = userRepository.save(request.toEntity(encoder.encode(request.getPassword())));
         return UserDto.builder()
                 .id(savedUser.getId())
                 .userName(savedUser.getUserName())
