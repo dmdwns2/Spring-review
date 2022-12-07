e11701c25702f2342dfabcef3f97761047718d43
dmdwns2 <ssreki1@naver.com>
2022-12-07 15:50:47 +0900

    VisitServiceTest JpaRepository DI

--- src/test/java/com/example/review/service/VisitServiceTest.java
+++ src/test/java/com/example/review/service/VisitServiceTest.java
@@ -1,14 +1,18 @@
 package com.example.review.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 class VisitServiceTest {
 
+    // disease에 사용될 String 명칭
     private static final String HEADACHE = "headache"; // 두통
     private static final String TOOTHACHE = "toothache"; // 치통
     private static final String DIABETES = "diabetes"; // 당뇨병
     private static final String COLD = "cold"; //감기
     private static final String BURN = "burn"; //화상
+
+    JpaRepository repository;
 
 
 
