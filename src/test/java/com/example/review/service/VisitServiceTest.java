1ac84c76db43ff6025a99844e03862ca6bd2f024
dmdwns2 <ssreki1@naver.com>
2022-12-07 15:48:24 +0900

    VisitServiceTest 질병에 사용될 String 추가

--- src/test/java/com/example/review/service/VisitServiceTest.java
+++ src/test/java/com/example/review/service/VisitServiceTest.java
@@ -1,10 +1,15 @@
 package com.example.review.service;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@WebMvcTest
 class VisitServiceTest {
+
+    private static final String HEADACHE = "headache"; // 두통
+    private static final String TOOTHACHE = "toothache"; // 치통
+    private static final String DIABETES = "diabetes"; // 당뇨병
+    private static final String COLD = "cold"; //감기
+    private static final String BURN = "burn"; //화상
+
+
 
 }
